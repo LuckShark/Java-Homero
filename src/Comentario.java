347d9e00edc97725ba7e2bcd6f58a338b3eba9fd
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-02-05 21:22:12 -0300

    First Commit

---
+++ src/Comentario.java
@@ -0,0 +1,15 @@
+/**
+ * JAVADOC - Comentário voltado à documentação
+ */
+
+public class Comentario {
+    public static void main(String[] args) {
+        //Comentário em linha
+        System.out.println();
+    }
+}
+/*
+*
+* Comentário em blocos -> faz /**/
+
+
